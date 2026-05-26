c2fce4a55917213b1917b906164506aa22e48c75
Furkan Jemil <furkanjemil54@gmail.com>
2026-05-26 12:06:01 +0300

    chore: adjust entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 set -eu
 
-npx prisma db push
+if [ "${SKIP_DB_PUSH:-false}" != "true" ]; then
+	npx prisma db push
+fi
+
+if [ "${WAIT_FOR_DB:-true}" = "true" ]; then
+	if [ -f ./scripts/wait-for-db.js ]; then
+		echo "Waiting for database to become available..."
+		node ./scripts/wait-for-db.js
+	else
+		echo "No wait-for-db script found; skipping wait."
+	fi
+fi
+
+if [ "$#" -gt 0 ]; then
+	exec "$@"
+fi
+
 exec npm run start
