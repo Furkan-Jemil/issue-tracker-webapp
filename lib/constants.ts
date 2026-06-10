77678593017860815dd6bbaf0f1b18bbf53309d4
Furkan Jemil <furkanjemil54@gmail.com>
2026-06-10 14:56:57 +0300

    refactor: remove legacy monolith endpoints and views

--- lib/constants.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_PAGE_SIZE = 15;
-export const ISSUES_PAGE_SIZE = 20;
--- lib/utils.ts
+++
@@ -1,7 +0,0 @@
-// Utility function for merging Tailwind classes (from shadcn/ui docs)
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
